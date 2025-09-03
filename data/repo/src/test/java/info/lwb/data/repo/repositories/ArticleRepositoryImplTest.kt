daa5fc925c3c1b4589377e57880ff9de7e22c364
mohammadmdp <mohammadmdp@gmail.com>
2025-09-04 02:36:49 +0330

    test: remove Room in-memory JVM-incompatible ArticleDaoTest (LWB-22 #time 3m)

--- data/repo/src/test/java/info/lwb/data/repo/repositories/ArticleRepositoryImplTest.kt
+++ data/repo/src/test/java/info/lwb/data/repo/repositories/ArticleRepositoryImplTest.kt
@@ -7,6 +7,7 @@ import info.lwb.data.network.ArticleApi
 import info.lwb.data.network.ManifestItemDto
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import info.lwb.core.common.Result
@@ -45,11 +46,17 @@ class ArticleRepositoryImplTest {
     @Test
     fun emitsLocalArticles() = runTest {
         dao.upsertArticle(ArticleEntity("id1","Title1","slug1",1,"now",100))
-        val first = repo.getArticles().first()
-        if (first is Result.Success) {
+        val emissions = mutableListOf<Result<*>>()
+        repo.getArticles().collect { value ->
+            emissions.add(value)
+            if (value is Result.Success<*>) return@collect
+        }
+        // Expect at least Loading then Success
+        val success = emissions.last()
+        if (success is Result.Success<*>) {
             @Suppress("UNCHECKED_CAST")
-            val list = (first as Result.Success<List<*>>).data
+            val list = success.data as List<*>
             assertEquals(1, list.size)
-        } else error("Expected Success result, got $first")
+        } else error("Expected final Success, got $success with emissions=$emissions")
     }
 }
