edc28f2e1927913b82f9011fef3ffbd7b162f9fb
mohammadmdp <mohammadmdp@gmail.com>
2025-09-04 02:29:17 +0330

    LWB-22 LWB-23 #time 0h 10m test(data): refine repository test, drop android-dependent DAO test

---
+++ data/repo/src/test/java/info/lwb/data/repo/repositories/ArticleRepositoryImplTest.kt
@@ -0,0 +1,55 @@
+package info.lwb.data.repo.repositories
+
+import info.lwb.data.repo.db.ArticleContentEntity
+import info.lwb.data.repo.db.ArticleDao
+import info.lwb.data.repo.db.ArticleEntity
+import info.lwb.data.network.ArticleApi
+import info.lwb.data.network.ManifestItemDto
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import info.lwb.core.common.Result
+import org.junit.Before
+import org.junit.Test
+
+class FakeArticleDao : ArticleDao {
+    private val articles = mutableMapOf<String, ArticleEntity>()
+    private val contents = mutableMapOf<String, ArticleContentEntity>()
+    override suspend fun getArticle(id: String) = articles[id]
+    override suspend fun listArticles() = articles.values.toList()
+    override suspend fun upsertArticle(article: ArticleEntity) { articles[article.id] = article }
+    override suspend fun upsertContent(content: ArticleContentEntity) { contents[content.articleId] = content }
+    override suspend fun upsertArticleWithContent(article: ArticleEntity, content: ArticleContentEntity) { upsertArticle(article); upsertContent(content) }
+    override suspend fun getContent(articleId: String) = contents[articleId]
+    override suspend fun getArticleContent(articleId: String) = contents[articleId]
+}
+
+class StubArticleApi : ArticleApi {
+    var manifest: List<ManifestItemDto> = emptyList()
+    override suspend fun getManifest(): List<ManifestItemDto> = manifest
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ArticleRepositoryImplTest {
+    private lateinit var repo: ArticleRepositoryImpl
+    private val dao = FakeArticleDao()
+    private val api = StubArticleApi()
+
+    @Before
+    fun setup() {
+        // simple repo that currently relies only on DAO for tests
+    repo = ArticleRepositoryImpl(api = api, articleDao = dao)
+    }
+
+    @Test
+    fun emitsLocalArticles() = runTest {
+        dao.upsertArticle(ArticleEntity("id1","Title1","slug1",1,"now",100))
+        val first = repo.getArticles().first()
+        if (first is Result.Success) {
+            @Suppress("UNCHECKED_CAST")
+            val list = (first as Result.Success<List<*>>).data
+            assertEquals(1, list.size)
+        } else error("Expected Success result, got $first")
+    }
+}
