74c4f1194030e560743a7bee6ff8f16cb156ca7f
mohammadmdp <mohammadmdp@gmail.com>
2025-09-17 02:59:41 +0330

    ops: restart lwb-server service as the next single-command step

--- linux_commands.sh
+++ linux_commands.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo sed -i 's/^ALTCHA_MAXNUMBER=.*/ALTCHA_MAXNUMBER=20000/' /etc/lwb-server.env || echo 'ALTCHA_MAXNUMBER=20000' | sudo tee -a /etc/lwb-server.env > /dev/null
+sudo systemctl restart lwb-server
