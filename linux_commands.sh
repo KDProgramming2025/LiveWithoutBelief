567259d44a12331adf60990eac149d335bdbfe8c
mohammadmdp <mohammadmdp@gmail.com>
2025-09-18 16:43:56 +0330

    admin(users): keep search input and button inline admin(articles): ensure grid layout, match file chooser styling to menu admin(textures): brushed-metal made seamless and less noisy admin(sidebar): restore collapse width and labels hiding

--- linux_commands.sh
+++ linux_commands.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /var/www/LWB && git --no-pager pull --ff-only
+cd /var/www/LWB && sudo -u www-data git fetch --all && sudo -u www-data git checkout feature/LWB-92-admin-ui && sudo -u www-data git --no-pager pull --ff-only
 
